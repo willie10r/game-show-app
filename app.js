2c338225680255f000089842d4f952183db28d91
willie m <zx10rwillie@gmail.com>
2022-06-29 18:53:15 -0500

    help

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const key = document.getElementById('qwerty');
 const phr =  document.getElementById('phrase');
-let rest = document.getElementsByClassName('btn_reset');
+const rest = document.getElementsByClassName("btn_reset");
 let missed = 0;
 const phrases = ['apex', 'enginebrake', 'gird', 'paddock', 'parabolica'];
 
-rest.addEventListener('click');
+ rest.addEventListener('click', e => {
+console.log("hello");
+
+});
+
