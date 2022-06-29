c2205c03bbffd3a64fc797a92a7be94ab6295ebe
willie m <zx10rwillie@gmail.com>
2022-06-28 20:17:24 -0500

    setting up var

---
+++ app.js
@@ -0,0 +1,6 @@
+const key = getElementById('qwerty');
+const phr = getElementById('phrase');
+const rest_btn = getElementByClassName('btn_reset');
+let missed = 0;
+const phrases = ['apex', 'enginebrake', 'gird', 'paddock', 'parabolica'];
+
