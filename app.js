e6168f51e3d87a2e992660257f7ab27a09fc20bd
willie m <zx10rwillie@gmail.com>
2022-06-29 20:00:37 -0500

    updated app.js

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const phr =  document.querySelector('#phrase');
 const rest = document.querySelector('.btn__reset');
 let missed = 0;
 let phrases = ['apex', 'enginebrake', 'gird', 'paddock', 'parabolica'];
+let ranNum = phrases[]
 
- rest.addEventListener('click', () => {
-
-    document.querySelector('#overlay').style.display = "none"
-console.log('what');
+rest.addEventListener('click', () => {
+  document.querySelector('#overlay').style.display = "none"
+  console.log('what');
 });
 
+function stub() {
+
+
+}
