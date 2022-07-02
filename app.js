2f2ca702700e0b25fb1374b661d065a8351ed86f
willie m <zx10rwillie@gmail.com>
2022-07-01 20:18:32 -0500

    ...

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const key = document.querySelector('#qwerty');
 const phr =  document.querySelector('#phrase');
 const rest = document.querySelector('.btn__reset');
+const a_z = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let missed = 0;
 let phrases = ['apex', 'enginebrake', 'gird', 'paddock', 'parabolica'];
 let ranNum = Math.floor(Math.random()*5);
 
+
 rest.addEventListener('click', () => {
   document.querySelector('#overlay').style.display = "none"
-  console.log(phrases[ranNum]);
+  
 });
 
 function getRandomPhraseAsArray() {
@@ -16,8 +18,14 @@ function getRandomPhraseAsArray() {
 return dis;
 
 }
+console.log(getRandomPhraseAsArray());
 
-function addPhraseToDisplay() {
 
+function addPhraseToDisplay(a_z) {
+ for(i = 0)  {
+  
+  let li = document.createElement('li')
+
+ }
 
 }
