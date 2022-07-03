a8b58603d7d25e24d1189d622a92b84386a0c895
willie m <zx10rwillie@gmail.com>
2022-07-03 16:26:16 -0500

    updating functions

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 const key = document.querySelector('#qwerty');
 const phr =  document.querySelector('#phrase');
 const rest = document.querySelector('.btn__reset');
-const a_z = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let missed = 0;
-let phrases = ['apex', 'enginebrake', 'gird', 'paddock', 'parabolica'];
+let phrases = ['pit stop', 'enginebrake', 'starting gird', 'paddock', 'parabolica'];
 let ranNum = Math.floor(Math.random()*5);
+let dis = '';
 
-
-rest.addEventListener('click', () => {
+rest.addEventListener('click', (e) => {
   document.querySelector('#overlay').style.display = "none"
   
 });
-
 function getRandomPhraseAsArray() {
-  let dis = phrases[ranNum];
-
-return dis;
-
-}
-console.log(getRandomPhraseAsArray());
-
-
-function addPhraseToDisplay(a_z) {
- for(i = 0)  {
-  
-  let li = document.createElement('li')
-
- }
-
-}
+  dis = phrases[ranNum];
+  return dis;
+};
+function addPhraseToDisplay(dis) {
+ for(let i = 0; i < dis.length; i++)  {
+  if(dis[i] === ' ') {
+    let ul = document.getElementById("phrase");
+    let li = document.createElement('li');
+    li.innerHTML = dis[i];
+    ul.appendChild(li);
+    li.classList.add('space');
+    console.log('space');
+    console.log(dis[i]);
+  } else {
+    let ul = document.getElementById("phrase");
+    let li = document.createElement('li');
+    li.innerHTML = dis[i];
+    ul.appendChild(li);
+    li.classList.add('letter');
+    console.log('letter');
+    console.log(dis[i]);
+  };
+ 
+ };
+};
+addPhraseToDisplay(dis)
